0595561cbb25d8620b9b34cb2521a7caf7ad13f2
DM <dm@DMs-MacBook-Pro.fios-router.home>
2020-06-03 10:04:05 -0400

    test_hashtable passing

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -1,3 +1,10 @@
+# class LinkedList:
+#     def __init__(self, key, value):
+#         self.key = key
+#         self.value = value
+#         self.next = None
+
+
 class HashTableEntry:
     """
     Hash Table entry, as a linked list node.
@@ -61,8 +68,32 @@ class HashTable:
 
         Implement this.
         """
+
+        # Find the hash index
+        # Search the list for the key
+        # If it's there, replace the value
+        # If it's not, append a new record to the list
+
         index = self.hash_index(key)
-        self.storage[index] = HashTableEntry(key, value)
+        current = self.storage[index]
+
+        if current is not None:
+            # loop to search the list for the key
+            while current is not None:
+                if current.key == key:
+                    # If it's there, replace the value
+                    current.value = value
+                    break
+
+                current = current.next
+            # If it's not there, append a new record to the list
+            else:
+                new_node = HashTableEntry(key, value)
+                new_node.next = self.storage[index]
+                self.storage[index] = new_node
+
+        else:
+            self.storage[index] = HashTableEntry(key, value)
 
     def delete(self, key):
         """
@@ -73,8 +104,28 @@ class HashTable:
         Implement this.
         """
 
+        # Find the hash index
+        # Search the list for the key
+        # If found, delete the node from the list, (return the node or value?)
+        # Else return None
+
+        # Find the hash index
         index = self.hash_index(key)
-        self.storage[index].value = None
+
+        # Search the list for the key
+        if self.storage[index] is not None:
+            # If found, delete the node from the list
+            if self.storage[index].key == key:
+                self.storage[index].value = None
+            else:
+                current = self.storage[index].next
+                while current is not None:
+                    if current.key == key:
+                        current.value = None
+                    current = current.next
+        # Else return None
+        else:
+            return None
 
     def get(self, key):
         """
@@ -84,8 +135,22 @@ class HashTable:
 
         Implement this.
         """
+
+        # Find the hash index
         index = self.hash_index(key)
-        return self.storage[index].value
+
+        # Search the list for the key
+        if self.storage[index].key == key:
+            # If found, return the value
+            return self.storage[index].value
+
+        else:
+            current = self.storage[index].next
+            while current is not None:
+                if current.key == key:
+                    return current.value
+                current = current.next
+        return None
 
     def resize(self):
         """
@@ -94,6 +159,28 @@ class HashTable:
 
         Implement this.
         """
+
+        # Step 1: make a new, bigger table/array
+        # Step 2: go through all the old elements, and hash into the new list
+
+        # make a new, bigger table/array
+        old_storage = self.storage
+        self.capacity *= 2
+        self.storage = [None] * self.capacity
+
+        # go through all the old elements
+        # hash into the new list
+        for item in old_storage:
+
+            if item is not None and item.next is None:
+                self.put(item.key, item.value)
+            if item is not None and item.next is not None:
+                current = item
+                while current.next is not None:
+                    self.put(current.key, current.value)
+                    current = current.next
+                self.put(current.key, current.value)
+        return
 
 
 if __name__ == "__main__":
